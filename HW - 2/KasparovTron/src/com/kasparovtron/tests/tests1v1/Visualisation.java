469a0bc92880790d40a1a3bec71902c0da680c6f
Jivko Uzunov <59103450+jivko25@users.noreply.github.com>
2020-01-27 16:39:40 +0200

    Update Visualisation.java

--- HW - 2/KasparovTron/src/com/kasparovtron/tests/tests1v1/Visualisation.java
+++ HW - 2/KasparovTron/src/com/kasparovtron/tests/tests1v1/Visualisation.java
@@ -3,30 +3,113 @@ package com.kasparovtron.tests.tests1v1;
 
 public class Visualisation {
 	
-	public static void MoveVisualisation(int RowV, int ColV ) {
-		int VarTool1 = 10-RowV;;
-		for(int i = 10;i>0; i--)
+	public static void MoveVisualisation(int x, int y, int a, int b) {
+		int BeforeX = a - 1;
+		int BetweenXY = x - a - 1;
+		int AfterX = 10 - x;
+		int BeforeY = x - 1;
+		int BetweenYX = a - x - 1;
+		int AfterY = 10 - a;
+		int m = 10-x;;
+		int n = 10-a;
+		if(y != b) {
+		for(int i = 9;i>0; i--)
 		{
-			if(i != ColV)
+			if(b != y) {
+			if(i != y)
 			{
 				System.out.print("- - - - - - - - - -");
 				System.out.println("");
 			}
-			else
+			if(i == y)
 			{
-				for(int k = 1;k<RowV;k++)
+				for(int k = 1;k<x;k++)
 				{
 					System.out.print("- ");
 				}
-				System.out.print("+ ");
-				for(int c = 0;VarTool1>c; c++)
+				System.out.print("1 ");
+				for(int c = 0;m>c; c++)
 				{
 					System.out.print("- ");
 				}
 				System.out.println("");
 			}
+			if(i == b)
+			{
+				for(int h = 1;h<a;h++)
+				{
+					System.out.print("- ");
+				}
+				System.out.print("2 ");
+				for(int p = 0;n>p; p++)
+				{
+					System.out.print("- ");
+				}
+				System.out.println("");
+			}
+			}
 		}
-		
+		}
+			if(y == b)
+			{
+				if(x > a) {
+				for(int ix = 10;ix>0; ix--)
+				{
+				if(ix != y)
+				{
+					System.out.print("- - - - - - - - - -");
+					System.out.println("");
+				}
+				if(ix == y) {
+				for(int q = 0;q<BeforeX;q++)
+				{
+					System.out.print("- ");
+				}
+				System.out.print("2 ");
+				
+				for(int p = 0;BetweenXY>p; p++)
+				{
+					System.out.print("- ");
+				}
+				System.out.print("1 ");
+				for(int t = 0;AfterX>t;t++)
+				{
+					System.out.print("- ");
+				}
+				System.out.println("");
+				}
+			}
+				System.out.println("");
+				}
+				if(a > x) {
+					for(int iy = 10;iy>0; iy--)
+					{
+					if(iy != y)
+					{
+						System.out.print("- - - - - - - - - -");
+						System.out.println("");
+					}
+					if(iy == y) {
+					for(int q = 0;q<BeforeY;q++)
+					{
+						System.out.print("- ");
+					}
+					System.out.print("1 ");
+					
+					for(int p = 0;BetweenYX>p; p++)
+					{
+						System.out.print("- ");
+					}
+					System.out.print("2 ");
+					for(int t = 0;AfterY>t;t++)
+					{
+						System.out.print("- ");
+					}
+					System.out.println("");
+					}
+				}
+					System.out.println("");
+					}
+		}
 	}
-
 }
